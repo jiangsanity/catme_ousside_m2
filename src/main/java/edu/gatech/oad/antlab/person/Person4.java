ab44e021bb8ed253c0940e44bf18c18320c7b27f
Rachel Lois Lecesne <rachellecesne@lawn-143-215-59-184.lawn.gatech.edu>
2019-01-24 11:26:05 -0500

    updated Person4.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Rachel
  *  @version 1.1
  */
 public class Person4 {
@@ -30,10 +30,14 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String newString = "";
+      for (int i = 0; i < input.length(); i++) {
+        char x = input.charAt(i);
+        x = x++;
+        newString += x;
+      }
+      return newString;
     }
-    
     /**
      * Return a string rep of this object
      * that varies with an input string
@@ -45,6 +49,5 @@ public class Person4 {
     public String toString(String input) {
       return name + calc(input);
     }
-
 }
 
